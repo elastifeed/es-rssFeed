38971611344f2384bd66b0a8f7dc20722d7f4f13
Jorge Crespo Sueiro <jorgecresposueiro@gmail.com>
2019-07-17 17:38:49 +0200

    argument bullshit

--- es-rssFeed/rssParser/__init__.py
+++ es-rssFeed/rssParser/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import DevelopmentConfig
 
 
-def create_app():
+def create_app(this):
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
     from rssParser.parser import data
